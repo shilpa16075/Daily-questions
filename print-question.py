4701aa8edb56ab5114634b8448538298314d2366
shilpa16075 <shilpa08500@gmail.com>
2026-03-22 10:53:31 +0530

    print statement

---
+++ print-question.py
@@ -0,0 +1,9 @@
+# In python we use print as a function 
+# So Hello World is a string. Anything that is single quotes or double quotes is called string.
+# If you start with single-quotes, then you must end with single quotes. The same holds true for double-quotes.
+print("Hello world")
+
+print('python's world')  
+# Here the single quotes is used three times; the single middle quote is a part of the english syntax, not programming syntax. However, the interpreter takes it as a programming syntax.
+# so you can use double quotes here.
+print ("python's world") 
