463e773b01fd2e5d55f54b2d114e2c169e13c717
shilpa16075 <shilpa08500@gmail.com>
2026-03-26 22:57:35 +0530

    break

--- conditional-statement-loops/termination-statement.py
+++ conditional-statement-loops/termination-statement.py
@@ -1,9 +1,18 @@
 #  Sometimes, depending on the situation, we want to terminate the loop in the middle of the iteration. The break statement allows us to break the loop at any point of the iteration.
 #  find the sum of the first n number, but if the sum is greater than 100, then stop the iteration
-sum = 0 
-num = int(input("enter the number: "))
-for each in range(1,num):
-    sum = sum+each 
-    if sum>100: 
+# sum1 = 0 
+# num = int(input("enter the number: "))
+# for each in range(1,num):
+#     sum1 = sum+each 
+#     if sum1>100: 
+#         break
+# print("total number is ", sum1)
+#  break statement within while loop 
+sum1 = 0 
+while True:
+    n = int(input("enter the number to add : or press 0 to exit "))
+    sum1 = sum1 + n 
+    print("sum1: ", sum1 )
+    if n ==0:
         break
-print("total number is ", sum)
+print("total is ", sum1)      
