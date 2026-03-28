3025a47d8dbc069d4c61b5baff17f2f364e68328
shilpa16075 <shilpa08500@gmail.com>
2026-03-28 22:38:10 +0530

    question

--- loops.py/question1.py
+++ loops.py/question1.py
@@ -1,4 +1,6 @@
-# Write a program to check whether a number is negative, postive and zero using if else loop.-
+# Write a program to check whether a number is negative, postive and zero using if else loop.
+
+
 num  = int(input("Enter a number: "))
 if num>0:
     print("Positive number")
--- loops.py/question4.py
+++ loops.py/question4.py
@@ -1,5 +1,5 @@
 # write a program to check whether the given year is leap year or not using if elif else.
-year = int(input("enter year"))
+year = int(input("enter year: "))
 if year%100==0 :
     if year%400==0:
         print('leap year')
