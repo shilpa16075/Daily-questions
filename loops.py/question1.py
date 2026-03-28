4c8bf7edf3b8efed5fec2f7ca4eeaf2bba0e83b0
shilpa16075 <shilpa08500@gmail.com>
2026-03-28 21:42:09 +0530

    question

---
+++ loops.py/question1.py
@@ -0,0 +1,8 @@
+# Write a program to check whether a number is negative, postive and zero using if else loop.-
+num  = int(input("Enter a number: "))
+if num>0:
+    print("Positive number")
+elif num ==0:
+    print("zero")
+else: 
+    print("Negative")
