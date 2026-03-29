ede7f0853b2360cd7966e79a9dfa53e2c83634b4
shilpa16075 <shilpa08500@gmail.com>
2026-03-29 18:42:49 +0530

    star pattern

---
+++ loops.py/pattern-ques.py
@@ -0,0 +1,23 @@
+#  write a program to print pattern using for and while loop 
+# *
+# **
+# ***
+# ****
+# *****     
+for  i in range (1,7):
+    print("*"*i)
+#  reverse the same pattern 
+for i in range(6,0,-1):
+    print("*"*i)
+
+
+n = 1
+while n<=6:
+    print("*"*n)   # print string * the value of n 
+    n=n+1          # increment to next value
+
+n = 6 
+while n>=0:
+    print("*"* n)
+    n = n-1
+
---
+++ loops.py/question6.py
@@ -0,0 +1,12 @@
+#  print a pattern 
+# *
+# **
+# ***
+# ****
+# *****
+for i in range(1,5):
+    print("*"* i)
+n = 1 
+while n<=6:
+    print("*" * n)
+    n = n + 1
