3723d10af3daf5a7c67870594d1477c6cf31b920
shilpa16075 <shilpa08500@gmail.com>
2026-03-29 21:43:15 +0530

    exiting the loop

--- loops.py/question7.py
+++ loops.py/question7.py
@@ -1,4 +1,6 @@
 # Write a program to take user input till the user type "exit" using a while loop.
-fruit_list = {"banana", "orange", "watermeon"}
-for i in fruit_list:
-    print(i)
+Enter = input("enter the input: ")
+while Enter!= "exit":
+    print("enter again ")
+    Enter = input("enter the input: ")
+print("sucessfully exited")
