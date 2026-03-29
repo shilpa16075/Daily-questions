af7d1f0e919af5012c8a33b406c99736d7317891
shilpa16075 <shilpa08500@gmail.com>
2026-03-29 19:43:07 +0530

    string instead of number

---
+++ loops.py/question8.py
@@ -0,0 +1,6 @@
+# Write a program to print number from 1 to 10 but print shilpa instead of number multiple of 5.
+for i in range(1,50):
+    if i%5==0:
+        print("shilpa")
+    else:
+        print(i)
