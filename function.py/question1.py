18f13b247e0937c35bb89910763162d1f3979c53
shilpa16075 <shilpa08500@gmail.com>
2026-03-31 21:29:42 +0530

    function

--- function.py/question1.py
+++ function.py/question1.py
@@ -1,9 +1,20 @@
 # write a program for sum of two number.
-def sumfun():
-    a = 10
-    b = 11
+def sumfun(a,b):
+    # a = 10
+    # b = 11
     sum = a + b
     print(sum)
-sumfun()
-sumfun()
-sumfun()
+    return "Shilpa"
+    print("end");
+
+square = lambda x:x**2
+
+def squarefun(x):
+    return x**2
+
+print(squarefun(3))
+
+result= sumfun(2,4)
+print(result)
+sumfun(6,7)
+sumfun(4,8)
