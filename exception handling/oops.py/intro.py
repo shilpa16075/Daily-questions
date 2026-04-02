83850756f0fba2330aa1d6e542493521371bf008
shilpa16075 <shilpa08500@gmail.com>
2026-04-02 22:14:02 +0530

    creating object

---
+++ exception handling/oops.py/intro.py
@@ -0,0 +1,10 @@
+class Students:
+    
+        name1 = "shilpa"
+        name2 = "saumya"
+        name3 = "chirag"
+        
+        
+obj1 = Students()
+print(Students.name1)
+print(Students.name3)
---
+++ function.py/prac.py
@@ -0,0 +1,15 @@
+# def add_numbers(a,b):
+#     print(a+b)
+#     print(a-b)
+
+# add_numbers(7,11)
+
+# def fav_food(food):
+#     print(f"shilpa loves {food}")
+
+# fav_food("momo")
+def multiply(a=10, b=10):
+    return a*b
+result=multiply(5,10)
+print(result)
+print(multiply(10,10))
