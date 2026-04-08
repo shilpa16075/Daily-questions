7c5ad5cf950ce55425693f2f780108428e1977c7
shilpa16075 <shilpa08500@gmail.com>
2026-04-08 23:14:37 +0530

    conflict resolved

---
+++ Daily-questions/age-datatype.py
@@ -0,0 +1,5 @@
+# Program to take age as input and print the value entered its data type
+name = input("enter your name: ")
+age = input("enter your age: ")
+print(type(name))
+print(type(age))
---
+++ Daily-questions/temperature-convertor.py
@@ -0,0 +1,3 @@
+# Smart temperature convertor
+a = float(input('enter the temprature in celsius: '))
+print(a+273.15)
