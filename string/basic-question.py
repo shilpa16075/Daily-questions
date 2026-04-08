7f2847e1f5912323d3a2bfacba819a3f78203fa4
shilpa16075 <shilpa08500@gmail.com>
2026-04-08 15:01:53 +0530

    string

--- string/basic-question.py
+++ string/basic-question.py
@@ -2,9 +2,3 @@
 # the last character and the length of the name.
 name = input("Enter you good name: ")
 print(name[0])   
-# Negative indexing
-print(name[-1])
-print(name[-2])
-# To access last characters of the unknown string we use str[-x:]
-print(name[-3:])
-print(len(name))
