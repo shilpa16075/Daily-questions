5d1e46e1849c03f9ebd0d38ec51bbf1e129aca57
shilpa16075 <shilpa08500@gmail.com>
2026-04-09 17:00:39 +0530

    common string methods

---
+++ string/string-method-question.py
@@ -0,0 +1,6 @@
+# Write a program that takes a input  sentence from the user and then convert it into lowercase ,replace all space
+# " " with the underscore(_). print the new string.
+str1 = input("Enter the sentence of your choice: ") 
+print(str1.lower())
+print(str1.upper())
+print(str1.replace(" ","_"))
