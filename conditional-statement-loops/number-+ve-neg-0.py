a68e3cfc6efb48a12394282c5fe1b6f5ddddbad0
shilpa16075 <shilpa08500@gmail.com>
2026-04-10 12:35:21 +0530

    Number is postive negative or zero

---
+++ conditional-statement-loops/number-+ve-neg-0.py
@@ -0,0 +1,8 @@
+# Write a program to check whether the number is postive,negative and zero and input form the user.
+num = int(input('enter number of your choice: '))
+if num>0:
+    print("Number is Postive!!!")
+elif num==0:
+    print("Number is equal to Zero")
+else:
+    print("Number is Negative")
