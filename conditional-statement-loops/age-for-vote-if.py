f052d1ec65e4a7aa254fff299e67e8794cac75ad
shilpa16075 <shilpa08500@gmail.com>
2026-04-10 12:39:23 +0530

    Age for vote

--- conditional-statement-loops/age-for-vote-if.py
+++ conditional-statement-loops/age-for-vote-if.py
@@ -1,6 +1,6 @@
 # If statement is used to run a block of code only when the condition is true.
 age = 18
 if age<=20:
-    print("Hello")
-    print("World")
+    print("Hello")    #Here the condition is true, print statement will be executed.
+    print("World")  # this statement has the same indentation which means this statement belong to if block: Executed too.
 print("Program ends")
