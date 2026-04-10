ad9b5eca425c802680009a4f77c4f607a332c1f5
shilpa16075 <shilpa08500@gmail.com>
2026-04-10 17:27:21 +0530

    for loop

---
+++ for-loops-questions.py/for-loops-basics.py
@@ -0,0 +1,6 @@
+#  program to demonstrate the use of for loop 
+n = int(input("enter the number of your choice: "))
+sum = 0
+for i in range(1,1+n):
+    sum+=i
+    print("sum of n",n,"natural numbers is :",sum)
