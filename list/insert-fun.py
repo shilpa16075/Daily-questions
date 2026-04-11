0933b239554ea0cfaee7744a86bfb7b3ff3f3e3d
shilpa16075 <shilpa08500@gmail.com>
2026-04-11 13:15:55 +0530

    list

---
+++ list/insert-fun.py
@@ -0,0 +1,11 @@
+# insert function is used to add an element at a specific index
+# n = int(input("enter a number: "))
+# number_list = []
+# for i in range(n):
+#     a = int(input("enter values for the list: "))
+#     number_list.insert(i,a)
+# print("entered list",number_list)
+
+n = ["shilpa","riya","ritika"]
+n.insert(2,"diya")
+print(n)
---
+++ list/range-fun.py
@@ -0,0 +1,4 @@
+# program to enter a value in list using range function
+x = int(input("enter any number for range: "))
+l = list(range(x))
+print(l)
