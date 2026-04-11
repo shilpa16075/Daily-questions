b669d11a16e0f96f6764952ad989171ed8fd20b6
shilpa16075 <shilpa08500@gmail.com>
2026-04-11 11:44:57 +0530

    for loop

--- for-loops-questions.py/for-loops-basics.py
+++ for-loops-questions.py/for-loops-basics.py
@@ -1,6 +1,6 @@
 #  program to demonstrate the use of for loop 
-n = int(input("enter the number of your choice: "))
-sum = 0
-for i in range(1,1+n):
+n = int(input("enter the number of your choice: "))    #suppose n = 7
+sum = 0                                                #sum of first natural number is 1 
+for i in range(1,1+n):                                 #range(1,8) because end element is not included but we need sum upto the value n  
     sum+=i
     print("sum of n",n,"natural numbers is :",sum)
--- for-loops-questions.py/nested-for-loop.py
+++ for-loops-questions.py/nested-for-loop.py
@@ -3,4 +3,4 @@ list2 = [4,5,6]
 for i in list:
     for j in list2:
         print(i,j)
-    print()    
+    print()  
