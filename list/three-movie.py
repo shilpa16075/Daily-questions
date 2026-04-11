42539d83166774b3743ec5c577354134442d0a4c
shilpa16075 <shilpa08500@gmail.com>
2026-04-11 14:03:42 +0530

    list operation

---
+++ list/three-movie.py
@@ -0,0 +1,15 @@
+# Write a program that stores the names of three of your favorite movies in a list called movies.
+
+# Then:
+
+# Print the entire list
+# Print the first and last movie in the list
+# Change the second movie to a new title of your choice
+# Print the updated list
+
+movie_list = ['Dhurander','Thor','Avengers']
+print(movie_list)
+print('first movie:',movie_list[0])
+print('last movie:',movie_list[-1])
+movie_list[1] = 'Iron Man'
+print(movie_list)
