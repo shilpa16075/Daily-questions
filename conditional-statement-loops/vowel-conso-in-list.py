dce090bfab3cd33e99463a86572f5dbf4954dcd4
shilpa16075 <shilpa08500@gmail.com>
2026-04-13 19:01:41 +0530

    vowel

---
+++ conditional-statement-loops/vowel-conso-in-list.py
@@ -0,0 +1,6 @@
+# Check if the character is vowel or consonante
+a = input('Enter a Character: ')
+if a in ['a','e','i','o','u','A','E','I','O','U']:
+    print('Character is Vowel')
+else:
+    print('Character is a Consonant!!!!!')
