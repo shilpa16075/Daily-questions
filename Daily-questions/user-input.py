f8b90fdbd074276040d93499e5bd8670a4cb722a
shilpa16075 <shilpa08500@gmail.com>
2026-04-13 12:45:18 +0530

    learning

---
+++ Daily-questions/user-input.py
@@ -0,0 +1,5 @@
+# Write a program that accepts two integer numbers from the user and calculates their multiplication.
+# Print the final result to the console.
+user1= int(input("Enter the number of your choice: "))
+user2 = int(input("Enter the second number: "))
+print(user1*user2)
---
+++ function.py/pattern.py
@@ -0,0 +1,9 @@
+# Program to print the pattern.      #i [1,2,3,4,5]
+for j in range(4): #j[2,3,4,5]
+    for i in range(j+1):
+        print("*", end=" ")
+    print()
+    
+
+
+
