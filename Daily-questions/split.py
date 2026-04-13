75d72298324fbaa6554415dc1f4c4f7b992277ee
shilpa16075 <shilpa08500@gmail.com>
2026-04-13 13:18:29 +0530

    split function

---
+++ Daily-questions/split.py
@@ -0,0 +1,5 @@
+str1 ,str2,str3 = input("enter the three name: ").split()
+# split() string ko list mein convert karta hai based on separator.
+print("name1:",str1)
+print("name2",str2)
+print("name3",str3)
