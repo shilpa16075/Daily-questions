e536a4802a14a8a5a2e3e265a39d9bfe775aa365
shilpa16075 <shilpa08500@gmail.com>
2026-04-13 13:30:59 +0530

    format specifiers

---
+++ Daily-questions/decimal_%f.py
@@ -0,0 +1,4 @@
+#  Display a float number (a number with decimals) such as 458.541315 rounded to exactly two decimal places.
+a = float(input("enter the decimal value: "))
+# %.2f is used to print a float value up to 2 decimal places.
+print('%.2f'%a)
