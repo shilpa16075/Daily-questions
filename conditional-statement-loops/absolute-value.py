c506e38086818d4879e96a79b4faa73fde146f6a
shilpa16075 <shilpa08500@gmail.com>
2026-04-15 12:33:08 +0530

    Absolute value

---
+++ conditional-statement-loops/absolute-value.py
@@ -0,0 +1,6 @@
+# Find the absolute value of a number.
+num = int(input('Enter the value: '))
+if num>=0:
+    print(f'The absolute value of {num} is:{num}')
+else:
+    print(f'The absolute value of {num} is:{-num}')
