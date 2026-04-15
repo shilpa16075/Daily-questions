305c763680bb9f5f29b60c9cc6a6159b8aff1ff7
shilpa16075 <shilpa08500@gmail.com>
2026-04-15 11:51:02 +0530

    palindrome

--- conditional-statement-loops/palinedrome-string.py
+++ conditional-statement-loops/palinedrome-string.py
@@ -1,6 +1,6 @@
 # Check if the string is palinedrome
 word = input('Enter the word of your choice: ')
 if word ==word[::-1]:
-    print("It's a palinedrome")
+    print("It's a palindrome")
 else:
-    print("It's not a palinedrome!!!")
+    print("It's not a palindrome!!!")
