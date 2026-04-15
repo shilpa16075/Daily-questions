b3608a53ed77e528968d61fc1698f6acf78a17da
shilpa16075 <shilpa08500@gmail.com>
2026-04-15 11:11:34 +0530

    comparision of two

---
+++ conditional-statement-loops/smallest-of-3.py
@@ -0,0 +1,10 @@
+# Find the smallest of 3
+num1 = int(input('enter the number: '))
+num2 = int(input('enter the number: '))
+num3 = int(input('enter the number: '))
+if num1<=num2 and num1<=num3:
+    print('num1 is the smallest')
+elif num2<=num1 and num2<=num3:
+    print('num2 is the smallest')
+else:
+    print('num3 is smallest')
