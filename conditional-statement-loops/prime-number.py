b4aa25df9e147453c65edcd737a991077a25d16a
shilpa16075 <shilpa08500@gmail.com>
2026-04-15 10:46:21 +0530

    prime number

--- conditional-statement-loops/prime-number.py
+++ conditional-statement-loops/prime-number.py
@@ -1,10 +1,10 @@
 # Check if the number is prime number or not.
-num = int(input('enter the number of your choice: '))
+num = int(input('enter the number of your choice: '))       #let's suppose num = 7
 count= 0
-for i in range(1,num+1):
-    if num%i==0:
-        count+=1
-if count == 2:
+for i in range(1,num+1):         # range(1,2,3,4,5,6,7)
+    if num%i==0:                 # this will check if any number from the range is dividing the num or not.
+        count+=1                 # increment if factor is found
+if count == 2:                   # if factor are equal to 2 then num is prime 
     print('prime number')
-else:
+else:                            # if the count means the factor are more than 2 then num is not a prime number
      print('not a prime number')        
