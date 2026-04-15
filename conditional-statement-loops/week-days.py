5b0b5bf86c48c5bb63641574a342f163b8d32e6a
shilpa16075 <shilpa08500@gmail.com>
2026-04-15 14:46:15 +0530

    rename

---
+++ conditional-statement-loops/week-days.py
@@ -0,0 +1,36 @@
+# Display the days of the week for a given numbers.
+# day_num = int(input('Enter the day number you want to check: '))
+# if day_num==1:
+#     print('Moday')
+# elif day_num==2:
+#     print('Tuesday')
+# elif day_num==3:
+#     print('Wednesday')
+# elif day_num==4:
+#     print('Thursday')
+# elif day_num==5:
+#     print('Friday')
+# elif day_num==6:
+#     print('Saturday')
+# elif day_num==6:
+#     print('Sunday')
+# else:
+#     print('Invalid Number!!!!!')
+
+
+day_name = int(input('enter the day you want to check: ')) 
+match day_name:
+    case 1:
+        print('Moday')
+    case 2:
+        print('Tuesday')
+    case 3:
+        print('Wednesday')
+    case 4:  
+        print('Thursday')      
+    case 5:
+        print('Friday')
+    case 6:
+        print('Saturday')
+    case 7: 
+        print('Sunday')   
