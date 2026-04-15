972e7e574b4162aa2b7119fa54faafbd3b4d82bf
shilpa16075 <shilpa08500@gmail.com>
2026-04-15 12:02:47 +0530

    palindrome

--- conditional-statement-loops/palinedrome-string.py
+++ conditional-statement-loops/palinedrome-string.py
@@ -1,6 +1,6 @@
 # Check if the string is palindrome
 word = input('Enter the word of your choice: ')
-clean_word = word.replace(" ","").lower()
+clean_word = word.replace(" ","").lower()  # remove space(nurses run) and convert into lower or uppercase 
 if clean_word ==clean_word[::-1]:
     print("It's a palindrome")
 else:
