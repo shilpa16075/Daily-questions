0173f8b36c0c0d6bfb31047914fc36ae56c4974f
shilpa16075 <shilpa08500@gmail.com>
2026-04-15 10:52:31 +0530

    divisible of 3

---
+++ conditional-statement-loops/divisible-of-3.py
@@ -0,0 +1,6 @@
+#  Check if the number is multiple of 3.
+num = int(input('Enter number of your choice: '))
+if num%3==0:
+    print('Number is multiple of 3')
+else:
+    print('Number is not multiple of 3')
