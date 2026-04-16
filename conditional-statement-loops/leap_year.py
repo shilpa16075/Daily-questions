36b972bbaf79c737e0a99a3f93c6abe0e4777b13
shilpa16075 <shilpa08500@gmail.com>
2026-04-16 23:41:11 +0530

    leap year

--- conditional-statement-loops/leap_year.py
+++ conditional-statement-loops/leap_year.py
@@ -1,6 +1,6 @@
 # check if the year is leap year or not
-a = int(input('Enter the year you want to check:  '))
-if((a%400==0 )or (a%4==0) and a%100!=0 ):
-    print('leap year: ')
-else: 
-    print('not a leap year')
+year = int(input('Enter the year you want to check: '))
+if year%4==0 and year%100!=0 or year%400==0:        # century year is not a leap year unless it is divible by 400
+    print(f'{year} is a leap year')
+else:
+    print(f'{year} not a leap year')
