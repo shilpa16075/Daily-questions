9643704772356f434e8d6c119b89931c792144d1
shilpa16075 <shilpa08500@gmail.com>
2026-04-20 12:08:18 +0530

    printing list elements

--- for-loops-questions.py/display-list-items.py
+++ for-loops-questions.py/display-list-items.py
@@ -1,6 +1,5 @@
 # write a python program to print items of the list.
 # table generator
-x = int(input('enter the value for table: '))
-for i in range(1,11):
-    # multi = i*x
-    print(f'{x} x {i} = {i*x}')
+num_list = [1,2,3,4,5,6,7]
+for num in num_list:
+    print(num)
