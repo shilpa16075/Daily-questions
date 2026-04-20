8965ef722331d425b6e8adc0c46cd1df046a9d3d
shilpa16075 <shilpa08500@gmail.com>
2026-04-20 11:36:15 +0530

    even numbers within the given range

--- for-loops-questions.py/question1.py
+++ for-loops-questions.py/question1.py
@@ -1,3 +1,4 @@
-#  Using for and range() write a program to print all even numbers between 1 to 20
-for i in range(2,20,2):
+#  Using for and range() write a program to print all even numbers within the given range
+x = int(input('enter the range value: '))
+for i in range(2,x,2):
     print(i)
