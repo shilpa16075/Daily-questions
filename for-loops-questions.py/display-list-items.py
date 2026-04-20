8f891f4404c1262bb786fb2f96dcd1847d93d89c
shilpa16075 <shilpa08500@gmail.com>
2026-04-20 12:03:31 +0530

    table generator

---
+++ for-loops-questions.py/display-list-items.py
@@ -0,0 +1,6 @@
+# write a python program to print items of the list.
+# table generator
+x = int(input('enter the value for table: '))
+for i in range(1,11):
+    # multi = i*x
+    print(f'{x} x {i} = {i*x}')
