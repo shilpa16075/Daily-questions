3e5e044089810ad7b8b01a07027e3172371f91b5
shilpa16075 <shilpa08500@gmail.com>
2026-04-20 11:42:54 +0530

    sum of the number with a given range

--- for-loops-questions.py/sum-of-number.py
+++ for-loops-questions.py/sum-of-number.py
@@ -1,2 +1,6 @@
-# You’re given a list of numbers.
-# Write a for loop that prints the square of each number.
+# program to print the sum of all numbers from 1 to given number.
+x = int(input('Enter the range value: '))
+sum = 0
+for i in range(1,x):
+    sum = sum +i
+    print(sum)
