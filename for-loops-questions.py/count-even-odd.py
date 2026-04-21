7d43c27ea835fafee5dfa4607d249289270aff14
shilpa16075 <shilpa08500@gmail.com>
2026-04-21 15:40:41 +0530

    count from series

---
+++ for-loops-questions.py/count-even-odd.py
@@ -0,0 +1,11 @@
+# Write a python program to count the number of even and odd numbers from the series.
+list_num = [1,3,2,45,6,74,56,35,64,23]
+even = 0
+odd = 0
+for i in list_num:
+    if i %2==0:
+        even+=1
+    else:
+        odd+=1
+print('total even no in series are:',even)
+print('total odd number in series are:',odd)
