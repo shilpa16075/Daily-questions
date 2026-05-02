f1535de95c1234aec98afa63050f096bfc73e427
shilpa16075 <shilpa08500@gmail.com>
2026-05-02 21:59:07 +0530

    first odd

--- while loop/first.odd.py
+++ while loop/first.odd.py
@@ -1,7 +1,10 @@
 # write a program to print first 10 odd numbers.
 number = 1
-while number<30:
+count = 0
+while count<10:
     if number%2!=0:
         print(number)
+        count = count+1
+
     number+=1
    
