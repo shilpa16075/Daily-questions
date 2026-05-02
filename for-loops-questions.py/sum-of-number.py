df912373aaab2154c7f33d9e1128ee8eae092697
shilpa16075 <shilpa08500@gmail.com>
2026-05-02 22:22:06 +0530

    sum is a built in function

--- for-loops-questions.py/sum-of-number.py
+++ for-loops-questions.py/sum-of-number.py
@@ -1,6 +1,6 @@
 # program to print the sum of all numbers from 1 to given number.
 x = int(input('Enter the range value: '))
-sum = 0
+total_sum = 0
 for i in range(1,x):
-    sum = sum +i
-    print(sum)
+    total_sum = total_sum +i
+    print(total_sum)
