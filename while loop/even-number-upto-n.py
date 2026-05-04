a553c4ff196153ffa709c29ef3f1969bdd1b1a16
shilpa16075 <shilpa08500@gmail.com>
2026-05-04 21:23:44 +0530

    even upto n

--- while loop/even-number-upto-n.py
+++ while loop/even-number-upto-n.py
@@ -1,10 +1,10 @@
 # write a program to print even number upto n.
-# i = 0
-# n = int(input('Enter the value of n: '))
-# while i<=n:
-#     if i%2==0:
-#         print(i)
-#     i+=1
+i = 0
+n = int(input('Enter the value of n: '))
+while i<=n:
+    if i%2==0:
+        print(i)
+    i+=1
 
 # same program but with simple while.
 s = 0
