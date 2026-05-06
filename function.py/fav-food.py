0201241883baf02f16c355a7e36aecb4e8e00ad3
shilpa16075 <shilpa08500@gmail.com>
2026-05-06 21:00:43 +0530

    fav food

---
+++ function.py/fav-food.py
@@ -0,0 +1,6 @@
+# write a python function of favourite food with formatted string.
+def fav_food(maggie):
+    food = input('Enter your favourite food: ')
+    print(f'My favourite food is {food} and i love it!!!')
+
+fav_food()
