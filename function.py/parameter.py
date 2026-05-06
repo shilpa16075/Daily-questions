8c380682bb2e8e3044b046a7d2369cd96fe94725
shilpa16075 <shilpa08500@gmail.com>
2026-05-06 22:21:03 +0530

    average of number

--- function.py/parameter.py
+++ function.py/parameter.py
@@ -1,8 +1,9 @@
 # Write a python function for average of the number.
 def average_number(a=2,b=7):
-    average = (a+b)/2
-    print(f'The average of {a} and {b} is {average}')
-
-average_number(5,7)
-average_number()
-average_number(34,90)
+    return (a+b)/2
+result = average_number(67,87)
+print(result)
+result1 = average_number(46)
+print(result1)
+result2 = average_number()
+print(result2)
---
+++ function.py/sum-of-2-number.py
@@ -0,0 +1,5 @@
+# Write a python function to print sum of 2 numbers.
+def number_sum(a=23,b=79):
+    return a+b
+result = number_sum(105,97)
+print(result) 
