2f395aef3b4abf50ff6ed5d539d207a4d9ee2ce5
shilpa16075 <shilpa08500@gmail.com>
2026-05-08 23:36:05 +0530

    reverse a string

--- function.py/reverse-string.py
+++ function.py/reverse-string.py
@@ -1,9 +1,1 @@
-def reverse_string(text):
-    rev = ""
-    for ch in text:
-        rev = ch + rev
-    return rev
-
-# Example usage
-s = input("Enter a string: ")
-print("Reversed string:", reverse_string(s))
+# write a python function to reverse a string.
