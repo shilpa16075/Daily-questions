2a1f14417f31842658476f0a900820557a326ebe
shilpa16075 <shilpa08500@gmail.com>
2026-05-10 00:38:37 +0530

    docstring

--- function.py/docstring-in-function.py
+++ function.py/docstring-in-function.py
@@ -1,6 +1,6 @@
 # Docstring in python is a very cool thing!!!
 def cool_things():
     """this function is to show you a cool python feature!!!"""
-    print('Hello my friend focus on the screen while calling the function for the magic')
+    print('Hello my friend focus on the screen while calling the function to see the magic')
 
 cool_things()
