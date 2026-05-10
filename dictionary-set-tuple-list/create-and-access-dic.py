d6deb5a09e62f94ed2c48eeae3493d6926db3274
shilpa16075 <shilpa08500@gmail.com>
2026-05-10 23:54:42 +0530

    practice task

---
+++ dictionary-set-tuple-list/create-and-access-dic.py
@@ -0,0 +1,8 @@
+# write a program to create a dictionary and access a specific item.
+student = {
+    "Name":'Shristi',
+    "Grade":2,
+    "Roll no":13
+}
+print(student['Grade'])
+print(student['Roll no'])
---
+++ dictionary-set-tuple-list/dic-intro.py
@@ -0,0 +1,14 @@
+# Basics of dictionary
+student_info = {
+    "name":"Ram",
+    "dept":"uiet",
+    "subject" :"cloud computing",
+    "age" : 20 
+}
+print(student_info["dept"])   # finding values with the help of key
+
+
+# finding key with the help of value
+for key,value in student_info.items():
+    if value =="Ram":
+        print(f'The key for the value pair ram is: {key}')
