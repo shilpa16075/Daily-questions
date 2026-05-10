a365c349a39c9fa661e2f66b86d258174b9d9ef6
shilpa16075 <shilpa08500@gmail.com>
2026-05-10 17:19:28 +0530

    args

--- function.py/agrs.py
+++ function.py/agrs.py
@@ -1,4 +1,5 @@
 def arbitary_para(*args):
+    print(args)     # all the parameter are stored in tuple and are not allowed to changed later.
     total = sum(args)
     print(f'The numbers recieved in the agrs are {args}')
     print(total)
