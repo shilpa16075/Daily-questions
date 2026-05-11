11ca5eda35f815797aa7d85518d1061d05b8490b
shilpa16075 <shilpa08500@gmail.com>
2026-05-12 00:06:44 +0530

    dict

--- dictionary-set-tuple-list/kwargs-in-dict.py
+++ dictionary-set-tuple-list/kwargs-in-dict.py
@@ -3,5 +3,5 @@ def Hostel_details(**info):
     for i,j in info.items():
         print(f'The hostel details are {i} : {j}')
 
-Hostel_details(hostel_name= 'Ganga' ,Total_seats= 360,Mess_no= 1, rooms= 200)
-Hostel_details(hostel_name= 'Ganga' ,warden ='Dr.priyanka', rooms= 200)
+Hostel_details(hostel_name= 'Ganga' ,Total_seats= 360,Mess_no= 1, rooms= 200) # function call use =
+Hostel_details(hostel_name= 'Ganga' ,warden ='Dr.priyanka', rooms= 200)       # using {}brackets use : colon
