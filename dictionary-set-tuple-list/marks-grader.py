83b7c29c4444a393bb0f7ae39b81187aba18d63b
shilpa16075 <shilpa08500@gmail.com>
2026-05-12 19:25:08 +0530

    marks and grader

--- dictionary-set-tuple-list/marks-grader.py
+++ dictionary-set-tuple-list/marks-grader.py
@@ -12,31 +12,19 @@ def student_grade(data):
     for i,j in data.items():
         if 91<=j<=100:
             grade = "A+"
-            print(f'Student {i} has {grade}')
-            marks_grade[i] = grade
         elif 81<=j<=90:
             grade = 'A'
-            print(f'Student {i} has {grade}')
-            marks_grade[i] = grade
         elif 71<=j<=80:
             grade = 'B'
-            print(f'Student {i} has {grade}')
-            marks_grade[i] = grade
         elif 61<=j<=70:
             grade = 'C'
-            print(f'Student {i} has {grade}')
-            marks_grade[i] = grade
         elif 51<=j<=60:
             grade = 'D'
-            print(f'Student {i} has {grade}')
-            marks_grade[i] = grade
         elif 41<=j<=50:
             grade = 'E'
-            print(f'Student {i} has {grade}')
-            marks_grade[i] = grade
         else:
             grade = 'Failed'
-            print(f'Student {i} has {grade}')
+        print(f'Student scored {grade} grade')
         marks_grade[i] = grade
     return marks_grade
 final_result= student_grade(student_marks)
