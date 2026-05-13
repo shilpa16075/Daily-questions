342f74f6fdb7fbcba815bef3ec345f0184c56357
shilpa16075 <shilpa08500@gmail.com>
2026-05-13 10:15:16 +0530

    nested operations

--- dictionary-set-tuple-list/nested-dict.py
+++ dictionary-set-tuple-list/nested-dict.py
@@ -3,14 +3,26 @@
 student_data = {
     "jenny" : {'marks': 78, 'age':20, 'subject':'pythno'},
     "harry" : {'marks': 95, 'age':21, 'subject':'wed designing'},
-    "Aarti" : {'marks': 67, 'age':20, 'subject': 'software design'}
+    "Aarti" : {'marks': 67, 'age':20, 'subject': 'software design'},
+    "failed": ['mohan','tripti','gaurav','sandeep']
 }
-print(student_data['harry']['subject'])       # access the specific data
-student_data['jenny']['email'] ='jenny123@gmail.com'   # to update the data
+# access the specific data
+print(student_data['harry']['subject']) 
+
+ # to update the data
+student_data['jenny']['email'] ='jenny123@gmail.com'  
 print(student_data['jenny'])
-del student_data['jenny']['email']   #delete the data
+
+#delete the data
+del student_data['jenny']['email']   
 print(student_data['jenny'])
+
+#pop fuction
 student_data['harry']['email'] = 'harry67@gmail.com'
 print(student_data['harry'])
-print(student_data['harry'].pop('email'))     #pop fuction
+print(student_data['harry'].pop('email'))     
 print(student_data)
+# list in a nested dictionary
+print(student_data['failed'])
+# accessing the list index
+print(student_data['failed'][0])
