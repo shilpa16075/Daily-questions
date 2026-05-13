6ca91a81355ef55fb1db1f716d72bdfd349b948a
shilpa16075 <shilpa08500@gmail.com>
2026-05-13 10:05:30 +0530

    nested dictionary

--- dictionary-set-tuple-list/nested-dict.py
+++ dictionary-set-tuple-list/nested-dict.py
@@ -10,3 +10,7 @@ student_data['jenny']['email'] ='jenny123@gmail.com'   # to update the data
 print(student_data['jenny'])
 del student_data['jenny']['email']   #delete the data
 print(student_data['jenny'])
+student_data['harry']['email'] = 'harry67@gmail.com'
+print(student_data['harry'])
+print(student_data['harry'].pop('email'))     #pop fuction
+print(student_data)
