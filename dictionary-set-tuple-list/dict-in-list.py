84bda893e005f1c45b4c42b97bf67622ed8b0c8b
shilpa16075 <shilpa08500@gmail.com>
2026-05-13 11:20:21 +0530

    dict in a list

--- dictionary-set-tuple-list/dict-in-list.py
+++ dictionary-set-tuple-list/dict-in-list.py
@@ -1,8 +1,23 @@
 # dictionary in a list 
 student_data =[
-    {'name':'ram','age':17,'class':'12th','marks':89},
-    {'name':'sohan','age':18,'class':'12th','marks' :94},
-    {'name':'rishi','age':17,'class':'12th','marks':87}
+    {
+        'name':'ram',
+        'age':17,
+        'class':'12th',
+        'marks':89
+        },
+    {
+        'name':'sohan',
+        'age':18,
+        'class':'12th'
+        ,'marks' :94
+        },
+    {
+        'name':'rishi',
+     'age':17,
+     'class':'12th'
+     ,'marks':87
+     }
 ]
 print(student_data[0])
 print(type(student_data))
