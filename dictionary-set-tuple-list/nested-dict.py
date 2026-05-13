b6203a9c4365c7c8873a9d4b3fe9c63152bd781b
shilpa16075 <shilpa08500@gmail.com>
2026-05-13 09:58:22 +0530

    nested dictionary

--- dictionary-set-tuple-list/nested-dict.py
+++ dictionary-set-tuple-list/nested-dict.py
@@ -1,2 +1,12 @@
 # An introduction of nested dictionary.
 # nested dictionary 
+student_data = {
+    "jenny" : {'marks': 78, 'age':20, 'subject':'pythno'},
+    "harry" : {'marks': 95, 'age':21, 'subject':'wed designing'},
+    "Aarti" : {'marks': 67, 'age':20, 'subject': 'software design'}
+}
+print(student_data['harry']['subject'])       # access the specific data
+student_data['jenny']['email'] ='jenny123@gmail.com'   # to update the data
+print(student_data['jenny'])
+del student_data['jenny']['email']   #delete the data
+print(student_data['jenny'])
