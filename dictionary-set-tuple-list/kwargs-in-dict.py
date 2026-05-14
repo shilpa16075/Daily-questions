e4113e4618616b8ff7426685e549c77eca51693e
shilpa16075 <shilpa08500@gmail.com>
2026-05-14 22:25:41 +0530

    kwargs

--- dictionary-set-tuple-list/kwargs-in-dict.py
+++ dictionary-set-tuple-list/kwargs-in-dict.py
@@ -5,3 +5,4 @@ def Hostel_details(**info):
 
 Hostel_details(hostel_name= 'Ganga' ,Total_seats= 360,Mess_no= 1, rooms= 200) # function call use =
 Hostel_details(hostel_name= 'Ganga' ,warden ='Dr.priyanka', rooms= 200)       # using {}brackets use : colon
+Hostel_details(hostel_name= 'Meghna', warden = 'Dr.kirti Desai', rooms = 160)
