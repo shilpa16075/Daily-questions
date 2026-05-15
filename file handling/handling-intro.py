b54f450ec0b6944ff7c1380ae397f5abb975aabd
shilpa16075 <shilpa08500@gmail.com>
2026-05-15 22:21:52 +0530

    file handling

--- file handling/handling-intro.py
+++ file handling/handling-intro.py
@@ -1,2 +1,7 @@
 # python can be used to perform operations on a file.(read and write)
 # type of files: text file (.txt , .docx,.log etc.) binary file (.mp4,.mov,.png,etc)
+f = open("demo.txt","r")
+data = f.read()
+print(data)
+print(type(data))
+f.close()
