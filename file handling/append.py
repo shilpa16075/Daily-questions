2a5fe071aaec0f8936ee778f43f6a99f0a94a684
shilpa16075 <shilpa08500@gmail.com>
2026-05-15 23:27:49 +0530

    file handling

--- file handling/append.py
+++ file handling/append.py
@@ -1,5 +1,6 @@
 # append mode : add at the end of the file
 file = open('demo.txt','a')   # append mode
-file.write(f"If there's something worst then failure, it's regret" )
+file.write("If there's something worst then failure, it's regret" )
 print(file)
 file.close()
+print('text successfully added ')
