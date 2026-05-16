fdb144cbb525b2d78433e254854d5db9dff3ff0d
shilpa16075 <shilpa08500@gmail.com>
2026-05-16 22:42:56 +0530

    oops

--- oops/class-object-intro.py
+++ oops/class-object-intro.py
@@ -1,6 +1,6 @@
 # class is a blueprint for creating an objects.
 # Creating a class 
-class student:
+class student:      # jo bhi features hume ek object k save krane hote hai vo hum class k andr likhenge
     name = 'karan'
 # creating object (instance)
 s1 = student()       # s1 is the object
