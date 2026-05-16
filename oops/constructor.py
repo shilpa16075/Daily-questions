4a218c0e470bf7d944b1bfa03f850ca9cc471eb5
shilpa16075 <shilpa08500@gmail.com>
2026-05-16 23:23:07 +0530

    constructor

--- oops/constructor.py
+++ oops/constructor.py
@@ -1,13 +1,15 @@
 class student:
-
+    # class attribute which are common for ever object
+    college_name = "Shri ram college"
     def __init__(self,fullname,marks):       # fullname = given parameter  #you can add more arguments like marks course etc
-        self.name = fullname 
-        self.marks = marks
+        self.name = fullname   # object attribute
+        self.marks = marks    # object attribute are non common attributes
            #self.name =  new name created in object
         print('adding new student in database')
 
 s1= student('shilon',45)
 print(s1.name,s1.marks)
+print(s1.college_name)
 
 s2 = student('divyansh',39)
 print(s2.name,s2.marks)
