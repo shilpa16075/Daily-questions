2713586fdde83503f2b5a05905caf86dd902a504
shilpa16075 <shilpa08500@gmail.com>
2026-05-16 23:02:21 +0530

    oops

--- oops/constructor.py
+++ oops/constructor.py
@@ -1,11 +1,13 @@
 class student:
 
-    def __init__(self,fullname):       # fullname = given parameter
-        self.name = fullname    #self.name =  new name created in object
+    def __init__(self,fullname,marks):       # fullname = given parameter  #you can add more arguments like marks course etc
+        self.name = fullname 
+        self.marks = marks
+           #self.name =  new name created in object
         print('adding new student in database')
 
-s1= student('shilon')
-print(s1.name)
+s1= student('shilon',45)
+print(s1.name,s1.marks)
 
-s2 = student('divyansh')
-print(s2.name)
+s2 = student('divyansh',39)
+print(s2.name,s2.marks)
