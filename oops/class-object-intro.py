83be925c00499f8d191c3eb4751e567324a24f79
shilpa16075 <shilpa08500@gmail.com>
2026-05-16 22:40:50 +0530

    oops

---
+++ oops/class-object-intro.py
@@ -0,0 +1,8 @@
+# class is a blueprint for creating an objects.
+# Creating a class 
+class student:
+    name = 'karan'
+# creating object (instance)
+s1 = student()       # s1 is the object
+print(s1)
+print(s1.name)
