3a5d45f1ffd6bdfacdb8c8fc4feebdfe92948250
shilpa16075 <shilpa08500@gmail.com>
2026-05-20 21:55:38 +0530

    binary file handling

---
+++ file handling/binary-file-handling.py
@@ -0,0 +1,7 @@
+# Binary file handling is used to read and write data in the form of raw bytes (0s and 1s) instead of plain text, 
+# which is essential for processing media like images, videos, and executables.
+with open(r"C:\Users\PC\Pictures\Screenshots\Screenshot 2026-05-17 213046.png",'rb') as source:
+    data = source.read()
+
+with open(r"C:\Users\PC\Pictures\Screenshots\Screenshot 2026-05-17 213046.png",'wb') as destination:
+    data = destination.write()
