05f089117dc65c4a9ca183f50fca4c1aba923a73
shilpa16075 <shilpa08500@gmail.com>
2026-05-22 21:35:50 +0530

    fucnction with 2 parameter

---
+++ function.py/function-with-2-parameters.py
@@ -0,0 +1,6 @@
+# write a function called demo() that accepts two parameters: a name and an age. 
+# The function should print these values directly to the console.
+def demo(name,age):
+    print(name,age)
+
+demo('shilpa',19)
