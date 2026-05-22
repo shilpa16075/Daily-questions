9e68cd3eeea795d742a2475f1338fe80604abaf8
shilpa16075 <shilpa08500@gmail.com>
2026-05-22 23:50:27 +0530

    nested function

---
+++ function.py/inner-function.py
@@ -0,0 +1,12 @@
+# Create an outer function that accepts two parameters, a and b. Inside, create an inner function that calculates
+#  the addition of a and b. The outer function should then add 5 to that sum and return the final result.
+def outer(a,b):
+    def addition(sum):
+        sum = a+b
+        # print('sum of a,b is',sum)
+        sum1 = sum +5
+        return sum,sum1
+
+    result = print(addition(1,2))
+
+
