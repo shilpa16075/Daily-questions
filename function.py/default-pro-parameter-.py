f296eb3cf964fa8420ab8f8036e6cb08414142e9
shilpa16075 <shilpa08500@gmail.com>
2026-05-23 22:28:54 +0530

    default parameter

--- function.py/default-pro-parameter-.py
+++ function.py/default-pro-parameter-.py
@@ -1,6 +1,6 @@
 # Create a function show_employee() that accepts an employee’s name and salary. 
 # If the salary is not provided in the function call, the function should automatically assign a default 
-# value of 9000.
+# value of 9000
 def show_employee(name,salary=9000):
     print(f'Employee name is {name} and the salary is {salary}')
 
