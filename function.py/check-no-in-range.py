2d3f9b6d3c4d6ce88c75c9fc9481f88c2f938ad3
shilpa16075 <shilpa08500@gmail.com>
2026-05-24 22:00:39 +0530

    number in range

--- function.py/check-no-in-range.py
+++ function.py/check-no-in-range.py
@@ -1,7 +1,10 @@
 # write a python function to check whether the number falls in the given range.
 def number_test(n):
     if n in range(6,16):
+        # print(list(range(6,16)))
         print(f'The number {n} falls in the given range')
     else:
-        print('Number not found!!!')
+        print(f'{n} is  not found!!!')
+    
 number_test(7)
+number_test(16)
