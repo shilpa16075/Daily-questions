678465fb6ebf1c5825ac95b358eb334fb61f3615
shilpa16075 <shilpa08500@gmail.com>
2026-06-01 23:49:58 +0530

    done

--- dictionary-set-tuple-list/dic-intro.py
+++ dictionary-set-tuple-list/dic-intro.py
@@ -5,10 +5,10 @@ student_info = {
     "subject" :"cloud computing",
     "age" : 20 
 }
-print(student_info["dept"])   # finding values with the help of key
+print(student_info["dept"])   # finding the values with the help of key
 
 
-# finding key with the help of value
+# finding the key with the help of value
 for key,value in student_info.items():
     if value =="Ram":
         print(f'The key for the value pair ram is: {key}')
