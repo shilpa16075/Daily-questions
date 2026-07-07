ae1cb43dafef199f4bff4042ef743832dcc49749
shilpa16075 <shilpa08500@gmail.com>
2026-07-07 11:52:18 +0530

    ride height

---
+++ iiit course exercise/ride_height.py
@@ -0,0 +1,8 @@
+height = int(input("enter your height: "))  # Change this to test!
+
+if height >=45:
+  print("You can ride! 🎢")
+else:
+  print('sorry too short')
+    # ADD print statement
+print('program executed')
