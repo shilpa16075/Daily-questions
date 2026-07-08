a033808f946bc1a21a125103b6f130feaec4a19d
shilpa16075 <shilpa08500@gmail.com>
2026-07-08 10:41:41 +0530

    mini project

---
+++ project.py/mad-libs-game.py
@@ -0,0 +1,11 @@
+# You fill blanks to create a silly story.
+name = input('Enter your name: ').lower()
+animal = input('Enter your pet name: ').lower()
+pet_color = input("Enter your pet's color: ").lower()
+food = input("Enter your pet's fav food: ").lower()
+# Now creating silly story and blanks
+print(f"Once upon a time,{name} had a pet {animal}.")
+print(f"The {animal} was {pet_color} and loved to eat {food}!")
+print(f"Every day, {name} and the {animal} would play together.")
+print(f"They lived happily ever after!")
+print('THE END!!!')
