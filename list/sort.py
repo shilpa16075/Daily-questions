05ec774b9e217155a93ac2d3cb856854d95510e9
shilpa16075 <shilpa08500@gmail.com>
2026-07-09 11:26:16 +0530

    sort,sorteed

---
+++ list/sort.py
@@ -0,0 +1,18 @@
+# sort sorted and count
+numbers = [3,7,2,9,5,8,1,3,7,2,9,5,8,1]
+print(numbers.count(8))
+nums = [3,7,2,9,5,8,1]
+print(nums)
+nums.sort()
+print(nums)
+nums.sort(reverse=True)
+print(nums)
+# nums is now[1,2,3,5,7,8,9]
+print(nums)
+new = sorted(nums)
+print(new)
+new = sorted(nums,reverse=True)
+print(nums)
+print(new)
+# new nums is []
+
