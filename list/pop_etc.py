655494368e294d7239f3a2ac9d94125d228f7afa
shilpa16075 <shilpa08500@gmail.com>
2026-07-09 11:14:40 +0530

    list func

---
+++ list/pop_etc.py
@@ -0,0 +1,7 @@
+fruits= ['apple','orange','mango','watermelon']
+print(fruits[2])
+print(fruits[-1])
+print(fruits.append('lichi'))
+print(fruits)
+fruits.pop(-4)  
+print(fruits)
---
+++ project.py/quiz.py
@@ -0,0 +1,5 @@
+fruits= ['apple','orange','mango','watermelon']
+print(fruits[2])
+print(fruits[-1])
+print(fruits.append('lichi'))
+print(fruits)
