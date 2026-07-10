d53cd10cdc2abdd571eff61c962464b94c717161
shilpa16075 <shilpa08500@gmail.com>
2026-07-10 11:07:15 +0530

    dict

--- dictionary-set-tuple-list/new.py
+++ dictionary-set-tuple-list/new.py
@@ -1,8 +1,19 @@
 intro = {
     "Name":"Shilpa",
     "Age":"21",
+    "City":"Jaipur",
     "Sport":"Badminton"
 }
+intro["day"]="Friday"
+print(intro['City'])
+print(intro)
+del intro['Name']
+print(intro)
+for i in intro.keys():    #to access the keys only
+    print(i)
+
+for i in intro.values():
+    print(i)
 print(intro["Age"])
 print(intro.get('state'))   # .get specify a default value if the key doesn't exist 
 print(intro.get('state','not exist'))  
