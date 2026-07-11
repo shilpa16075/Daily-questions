77ca34f212d1459f6ed9a64884f2e6f4dad31d1b
shilpa16075 <shilpa08500@gmail.com>
2026-07-11 21:18:50 +0530

    dictionary

--- dictionary-set-tuple-list/new.py
+++ dictionary-set-tuple-list/new.py
@@ -12,8 +12,11 @@ print(intro)
 for i in intro.keys():    #to access the keys only
     print(i)
 
-for i in intro.values():
+for i in intro.values():  #to access the value only
     print(i)
+
+for keys,values in intro.items():   # to access both 
+    print(f'{keys}:{values}')
 print(intro["Age"])
 print(intro.get('state'))   # .get specify a default value if the key doesn't exist 
 print(intro.get('state','not exist'))  
