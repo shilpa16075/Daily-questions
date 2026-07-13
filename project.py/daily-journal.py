bd73f8cab6ac6e655d441b3eff21e9bfff5fab74
shilpa16075 <shilpa08500@gmail.com>
2026-07-13 21:52:48 +0530

    mini journal

---
+++ project.py/daily-journal.py
@@ -0,0 +1,7 @@
+# 🏆 FINAL PROJECT: Daily Journal!
+# Build a journal app that:
+
+# Lets you write entries
+# Saves entries to a file with timestamps
+# Lets you read all past entries
+# Appends new entries without deleting old ones
