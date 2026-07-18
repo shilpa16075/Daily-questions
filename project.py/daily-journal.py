938cefa4f26a1fdecea43f78fb398ab2fe438a64
shilpa16075 <shilpa08500@gmail.com>
2026-07-18 21:01:16 +0530

    daily journal

--- project.py/daily-journal.py
+++ project.py/daily-journal.py
@@ -4,3 +4,6 @@
 # Saves entries to a file with timestamps
 # Lets you read all past entries
 # Appends new entries without deleting old ones
+entry1 = input('Enter you thoughts: ')
+entry2 = input("Enter what you was's your goal for total: ")
+entry3 = input("Enter your plans for today: ")
