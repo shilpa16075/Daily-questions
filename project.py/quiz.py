b0cc962f639483f7c0d7478792ba440879fccfaf
shilpa16075 <shilpa08500@gmail.com>
2026-07-19 22:13:15 +0530

    quiz

--- project.py/quiz.py
+++ project.py/quiz.py
@@ -1,2 +1,13 @@
 # create a python project to ask 4 question to the user and marks one one every correct answer and display a message
 # on every wrong aswer respectively.
+print('WELCOME TO THE QUIZ COMPETITION!!!!')
+marks = 0
+Question1 = input('what is the color of sky: ')
+if Question1 == 'blue':
+    print('well played!!!moving to next question')
+    marks +=1
+    print(f'Your score = {marks}')
+else:
+    print('wrong answer!!!')
+    print(f'Your score = {marks}')
+
